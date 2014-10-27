9250e4478c13ed7b99817df7f34cf68e49b3392a
arekkas <aeneas.rekkas@serlo.org>
2014-10-27 14:01:38 +0100

    Refactoring

--- src/uid/service.go
+++ src/uid/service.go
@@ -37,34 +37,7 @@ func (t *Service) Create(args *CreatorArguments, reply *Uid) error {
 
 	*reply = Uid(id)
 	fmt.Printf("Identifier %s (%d) created with params id=%d type=%d timestamp=%d count=%d\n", id, id, t.Id, args.Type, now, t.count)
-	fmt.Printf("Identifier %s (%d) created with params id=%d type=%d timestamp=%d count=%d\n", reply, reply, reply.Creator(), reply.Type(), reply.Timestamp(), reply.MicroTime())
+	fmt.Printf("Identifier %s (%d) created with params id=%d type=%d timestamp=%d count=%d\n", reply, reply, reply.NodeId(), reply.Type(), reply.Timestamp(), reply.MicroTime())
 
 	return nil
-}
-
-func (t *Service) createId(uidType, nodeId, timestamp, timestampCount int64) int64 {
-
-	// Highest bit is 0 for serialization algorithm //
-	// 0l << (64 - 1) |
-
-	// Highest 9 bits are type //
-	//(int64(args.Type) & 511) << (64 - 1 - 9)) |
-
-	// Next bit is empty so that the first two alphanumerics only depend on
-	// type and not also the source
-
-	// 0l << (64 - 1 - 9 -1) */
-	// Next 5 bits are source */
-	//(int64(t.Id & 31) << (64 - 1 - 9 - 1 - 5)) |
-
-	// Next 4 bytes are TS */
-	//((now & int64(0xFFFFFFFF)) << (64 - 1 - 9 - 1 - 5 - 32)) |
-
-	// Next 2 bytes are "MicroTime" */
-	//ID & int64(0xFFFF)
-
-	return ((uidType & int64(0x1FF)) << (64-1-9)) |
-		((nodeId & int64(0x1F)) << (64-1-9-1-5)) |
-		((timestamp & int64(0xFFFFFFFF)) << (64-1-9-1-5-32)) |
-		(timestampCount & int64(0xFFFF))
 }
--- src/uid/uid.go
+++ src/uid/uid.go
@@ -9,22 +9,52 @@ type Identifier interface {
 
 type Uid int64
 
-func (t Uid) Type() int16 {
+// New creates a new unique identifier taking into account
+// the uid's type, the node which created this uid, the timestamp
+// of the creation and an offset to ensure uniqueness
+func New(uidType int16, nodeId byte, timestamp, offset int64) int64 {
+
+	// Highest bit is 0 for serialization algorithm //
+	// 0l << (64 - 1) |
+
+	// Highest 9 bits are type //
+	//(int64(args.Type) & 511) << (64 - 1 - 9)) |
+
+	// Next bit is empty so that the first two alphanumerics only depend on
+	// type and not also the source
+
+	// 0l << (64 - 1 - 9 -1) */
+	// Next 5 bits are source */
+	//(int64(t.Id & 31) << (64 - 1 - 9 - 1 - 5)) |
+
+	// Next 4 bytes are TS */
+	//((now & int64(0xFFFFFFFF)) << (64 - 1 - 9 - 1 - 5 - 32)) |
+
+	// Next 2 bytes are "MicroTime" */
+	//ID & int64(0xFFFF)
+
+	return ((int64(uidType) & int64(0x1FF)) << (64-1-9)) |
+			((int64(nodeId) & int64(0x1F)) << (64-1-9-1-5)) |
+			((int64(timestamp) & int64(0xFFFFFFFF)) << (64-1-9-1-5-32)) |
+			(int64(offset) & int64(0xFFFF))
+}
+
+func (t *Uid) Type() int16 {
 	return int16((int64(t) >> (64 - 9 - 1)) & int64(0x1FF))
 }
 
-func (t Uid) Creator() byte {
+func (t *Uid) NodeId() byte {
 	return byte((int64(t) >> (64 - 1 - 9 - 1 - 5)) & int64(0x1F))
 }
 
-func (t Uid) Timestamp() int64 {
+func (t *Uid) Timestamp() int64 {
 	return ((int64(t) >> (64 - 1 - 9 - 1 - 5 - 32)) & int64(0xFFFFFFFF))
 }
 
-func (t Uid) MicroTime() int64 {
+func (t *Uid) Offset() int64 {
 	return (int64(t) & int64(0xFFFF))
 }
 
-func (t Uid) String() string {
+func (t *Uid) String() string {
 	return strconv.FormatInt(int64(t), 10)
 }
